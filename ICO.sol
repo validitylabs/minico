81130ca9a101a60be6113fa7cca2977e1e0111c8
Sebastian C. Bürgel <SCBuergel@users.noreply.github.com>
2018-07-02 11:33:30 +0200

    no compiler warnings

--- ICO.sol
+++ ICO.sol
@@ -1,12 +1,16 @@
+pragma solidity ^0.4.24;
+
 contract ICO {
     mapping(address => uint256) public balanceOf;
+    
+    address public beneficiary = msg.sender; // initialize beneficiary with address that deploys contract
 
-    function() payable {
+    function() public payable {
         balanceOf[msg.sender] += msg.value;
-        address(0x0).transfer(msg.value); // @TODO: replace address!
+        address(beneficiary).transfer(msg.value);
     }
     
-    function transfer(address to, uint256 amount) {
+    function transfer(address to, uint256 amount) public {
         require(balanceOf[msg.sender] >= amount);
         balanceOf[msg.sender] -= amount;
         balanceOf[to] += amount;
